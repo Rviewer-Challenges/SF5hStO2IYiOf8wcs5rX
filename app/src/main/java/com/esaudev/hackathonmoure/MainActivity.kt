a997793896af3619e7ca7cb966a934bb085dd0ea
Esau <yosoypawn@hotmail.com>
2022-09-06 14:53:56 -0500

    bottom nav

--- app/src/main/java/com/esaudev/hackathonmoure/MainActivity.kt
+++ app/src/main/java/com/esaudev/hackathonmoure/MainActivity.kt
@@ -2,10 +2,19 @@ package com.esaudev.hackathonmoure
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.esaudev.hackathonmoure.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bnvMainMenu.setupWithNavController(findNavController(R.id.fMainContent))
     }
 }
---
+++ app/src/main/java/com/esaudev/hackathonmoure/content/ContentFragment.kt
@@ -0,0 +1,34 @@
+package com.esaudev.hackathonmoure.content
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.esaudev.hackathonmoure.R
+import com.esaudev.hackathonmoure.databinding.FragmentContentBinding
+
+class ContentFragment : Fragment() {
+
+    private var _binding: FragmentContentBinding? = null
+    private val binding: FragmentContentBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentContentBinding
+            .inflate(inflater, container, false)
+            .apply { _binding = this }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.test.setOnClickListener { findNavController().navigate(R.id.action_contentFragment_to_detailFragment) }
+    }
+
+}
